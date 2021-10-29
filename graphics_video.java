7ac960813da16cb4c4a0431808d26c5929d27c83
Kashaf-Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2021-10-29 20:17:48 +0500

    Add files via upload

---
+++ graphics_video.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class graphics_video extends graphics_video {
+    private JPanel main;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton OKButton;
+
+    public graphics_video() {
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
